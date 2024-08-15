b46b6682e7d44a29ea2bcd6a7db14aac368e2e66
jjjju1 <164972555+jjjju1@users.noreply.github.com>
2024-08-15 17:22:31 +0900

    Title: 숫자 카운트, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240815/숫자 카운트/numeric-count.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_N 10
+
+int n;
+int a[MAX_N], b[MAX_N], c[MAX_N];
+
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i] >> b[i] >> c[i];
+	}
+
+	int cnt = 0;
+
+	for (int i = 1; i <= 9; i++) {
+		for (int j = 1; j <= 9; j++) {
+			for (int k = 1; k <= 9; k++) {
+				if (i == j || j == k || k == i) { continue; }
+				bool succeeded = true;
+
+				for (int q = 0; q < n; q++) {
+					int x = a[q] / 100;
+					int y = a[q] / 10 % 10;
+					int z = a[q] % 10;
+
+					int cnt1 = 0;
+					int cnt2 = 0;
+
+					if (x == i) { cnt1++; }
+					if (y == j) { cnt1++; }
+					if (z == k) { cnt1++; }
+					if (x == j || x == k) { cnt2++; }
+					if (y == i || y == k) { cnt2++; }
+					if (z == i || z == j) { cnt2++; }
+
+					if (cnt1 != b[q] || cnt2 != c[q]) {
+						succeeded = false;
+						break;
+					}
+				}
+				if (succeeded) { cnt++; }
+			}
+		}
+	}
+
+	cout << cnt << endl;
+
+	return 0;
+}
