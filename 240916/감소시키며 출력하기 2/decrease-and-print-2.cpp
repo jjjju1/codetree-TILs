0a66aadaea2ac406d71462ff40fa873717596c2e
jjjju1 <164972555+jjjju1@users.noreply.github.com>
2024-09-16 00:49:20 +0900

    Title: 감소시키며 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240916/감소시키며 출력하기 2/decrease-and-print-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	int i = 26;
+	while (i >= 10) {
+		cout << i-- << ' ';
+	}
+}
