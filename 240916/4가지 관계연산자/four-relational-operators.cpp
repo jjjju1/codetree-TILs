24b783681277d6c923b700a386ec0b46b34076d1
jjjju1 <164972555+jjjju1@users.noreply.github.com>
2024-09-16 00:04:57 +0900

    Title: 4가지 관계연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240916/4가지 관계연산자/four-relational-operators.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+
+	cout << (a >= b) << endl;
+	cout << (a > b) << endl;
+	cout << (b >= a) << endl;
+	cout << (b > a) << endl;
+}
