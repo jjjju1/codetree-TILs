5b15a6904ddf2baf4016dac30848b344b8510e3e
jjjju1 <164972555+jjjju1@users.noreply.github.com>
2024-09-15 23:37:56 +0900

    Title: 두수의 곱과 몫, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240915/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << a << " * " << b << " = " << a * b << endl;
+	cout << a << " / " << b << " = " << a / b << endl;
+}
