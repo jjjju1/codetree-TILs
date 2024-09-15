a09db7123b9efdb95570fc02578d49bc7745e659
jjjju1 <164972555+jjjju1@users.noreply.github.com>
2024-09-16 00:06:33 +0900

    Title: 범위 안의 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240916/범위 안의 수/number-in-range.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	int a;
+	cin >> a;
+
+	if (10 <= a && a <= 20) {
+		cout << "yes";
+	}
+	else {
+		cout << "no";
+	}
+}
