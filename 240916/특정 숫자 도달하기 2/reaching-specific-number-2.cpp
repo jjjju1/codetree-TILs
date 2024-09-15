42f5bb430c5bd7eaa1584e57479feca3fb043a9e
jjjju1 <164972555+jjjju1@users.noreply.github.com>
2024-09-16 00:42:05 +0900

    Title: 특정 숫자 도달하기 2, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240916/특정 숫자 도달하기 2/reaching-specific-number-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	for (int i = n; i >= 1; i--) {
+		cout << i << ' ';
+	}
+}
