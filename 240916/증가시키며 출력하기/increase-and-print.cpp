b7822739236201a651f221ca16efea3cf3896a76
jjjju1 <164972555+jjjju1@users.noreply.github.com>
2024-09-16 00:39:40 +0900

    Title: 증가시키며 출력하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240916/증가시키며 출력하기/increase-and-print.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	int n;
+	
+
+	for (int i = 5; i <=17; i += 2) {
+		cout << i << ' ';
+	}
+	
+}
