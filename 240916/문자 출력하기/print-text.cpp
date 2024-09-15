18db055c664fabb387681b61b7d120e808e5fffe
jjjju1 <164972555+jjjju1@users.noreply.github.com>
2024-09-16 00:50:52 +0900

    Title: 문자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240916/문자 출력하기/print-text.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	char ch;
+	cin >> ch;
+
+	for (int i = 0; i < 8; i++) {
+		cout << ch;
+	}
+}
