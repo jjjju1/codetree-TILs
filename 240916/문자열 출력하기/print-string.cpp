ddeca0caf47635f6645558b4c21be2b539330975
jjjju1 <164972555+jjjju1@users.noreply.github.com>
2024-09-16 00:51:14 +0900

    Title: 문자열 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240916/문자열 출력하기/print-string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cout << "LeebrosCode" << endl;
+	}
+}
