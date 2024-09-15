7ea896c62259fc9e4e6f8f1da6559d07c44d02d2
jjjju1 <164972555+jjjju1@users.noreply.github.com>
2024-09-16 00:45:32 +0900

    Title: 증가시키며 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240916/증가시키며 출력하기 2/increase-and-print-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int count = 10;
+	while (count <= 26) {
+		cout << count++ << ' ';
+	}
+}
