8a808d2f0871e7ab56d69fa142284dd2fa1e6ed3
jjjju1 <164972555+jjjju1@users.noreply.github.com>
2024-09-16 00:36:26 +0900

    Title: 차례로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240916/차례로 출력/print-in-order.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	for (int i = 5; i <= 17; i++) {
+		cout << i << ' ';
+	}
+	
+}
